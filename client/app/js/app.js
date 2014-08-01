04e6292ecf8f571e5818a11de3ad12ba2bd2824e
Ben Florin <benjamin.florin@bc.edu>
2014-08-01 12:13:48 -0400

    Added deployment script and added submit-on-enter

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,18 +1,17 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers',
+(function () {
+    angular.module('myApp', [
+        'ngRoute',
+        'myApp.controllers',
         'ui.bootstrap'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/bento', {templateUrl: 'partials/bento.html', controller: 'bento'});
-  $routeProvider.when('/catalog', {templateUrl: 'partials/catalog.html', controller: 'catalog'});
-  $routeProvider.when('/databases', {templateUrl: 'partials/databases.html', controller: 'databases'});
-  $routeProvider.when('/articles', {templateUrl: 'partials/articles.html', controller: 'articles'});
-  $routeProvider.otherwise({redirectTo: '/bento'});
-}]);
+    ]).
+        config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/bento', {templateUrl: 'partials/bento.html', controller: 'bento'});
+            $routeProvider.when('/catalog', {templateUrl: 'partials/catalog.html', controller: 'catalog'});
+            $routeProvider.when('/databases', {templateUrl: 'partials/databases.html', controller: 'databases'});
+            $routeProvider.when('/articles', {templateUrl: 'partials/articles.html', controller: 'articles'});
+            $routeProvider.otherwise({redirectTo: '/bento'});
+        }]);
+})();
--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -1,74 +1,83 @@
 'use strict';
 
 /* Controllers */
+(function () {
 
-angular.module('myApp.controllers', []).
-    controller('bento', ['$http', '$scope', '$location',
+    angular.module('myApp.controllers', []).
+        controller('bento', ['$http', '$scope', '$location',
 
-        function ($http, $scope, $location) {
+            function ($http, $scope, $location) {
 
-            var base_url, search, search_services;
+                var base_url, search, search_services;
 
-            // Add new services here.
-            search_services = ['catalog', 'articles', 'dpla', 'services', 'guides'];
+                // Add new services here.
+                search_services = ['catalog', 'articles', 'dpla', 'services', 'guides'];
 
-            if ($location.search().any) {
+                if ($location.search().any) {
 
-                base_url = '/search-services/';
-                search = '?any=' + $location.search().any;
+                    base_url = '/search-services/';
+                    search = '?any=' + $location.search().any;
 
-                for (var i = 0; i < search_services.length; i++) {
-                    fetch(search_services[i]);
-                }
-            } else {
-                for (var i = 0; i < search_services.length; i++) {
-                    $scope[search_services[i] + '_results'] = [];
-                }
-            }
-
-            function fetch(search_service) {
-                $http.get(base_url + search_service + search, {'cache': true}).success(
-                    function (data) {
-                        console.log(search_service);
-                        console.log(data);
-                        $scope[search_service + '_results'] = data;
+                    for (var i = 0; i < search_services.length; i++) {
+                        fetch(search_services[i]);
                     }
-                ).error(
-                    function (data, status) {
-                        console.log(status);
+                } else {
+                    for (var i = 0; i < search_services.length; i++) {
+                        $scope[search_services[i] + '_results'] = [];
                     }
-                );
-            }
-        }])
-    .controller('AutoComplete', [ '$scope', '$http',
-        function ($scope, $http) {
-            $scope.selected = undefined;
+                }
 
-            $scope.getLocation = function (val) {
-                return $http.get('/search-services/suggest?text=' + val).then(function (res) {
-                    var suggestions = [];
-                    console.log(res.data.ac[0]);
-                    angular.forEach(res.data.ac[0].options, function (item) {
-                        suggestions.push(item);
+                function fetch(search_service) {
+                    $http.get(base_url + search_service + search, {'cache': true}).success(
+                        function (data) {
+                            console.log(search_service);
+                            console.log(data);
+                            $scope[search_service + '_results'] = data;
+                        }
+                    ).error(
+                        function (data, status) {
+                            console.log(status);
+                        }
+                    );
+                }
+            }])
+        .controller('AutoComplete', ['$scope', '$http',
+            function ($scope, $http) {
+                $scope.search = function ($item, $model, $label) {
+                    document.getElementById('searchbox').value = $item.text;
+                    search();
+                }
+
+                $scope.getLocation = function (val) {
+                    return $http.get('/search-services/suggest?text=' + val).then(function (res) {
+
+                        // Autopopulate first item
+                        var first_item = document.getElementById('searchbox').value;
+
+                        var suggestions = [{text: first_item, payload: {type: ""}}];
+                        angular.forEach(res.data.ac[0].options, function (item) {
+                            suggestions.push(item);
+                        });
+                        console.log(suggestions);
+                        return suggestions;
                     });
-                    return suggestions;
-                });
-            };
+                };
 
-        }])
-    .controller('catalog', ['$scope',
-        function ($scope) {
+            }])
+        .controller('catalog', ['$scope',
+            function ($scope) {
 
-        }]);
+            }]);
 
-String.prototype.truncate = function (max_length) {
-    var too_long, s_;
-    too_long = this.length > max_length;
-    if (too_long) {
-        s_ = this.substr(0, max_length - 1);
-        s_ = s_.substr(0, s_.lastIndexOf(' '));
-    } else {
-        s_ = this;
+    String.prototype.truncate = function (max_length) {
+        var too_long, s_;
+        too_long = this.length > max_length;
+        if (too_long) {
+            s_ = this.substr(0, max_length - 1);
+            s_ = s_.substr(0, s_.lastIndexOf(' '));
+        } else {
+            s_ = this;
+        }
+        return s_ + "…";
     }
-    return s_ + "…";
-}
+})();
