94bb38e9d5522c99daf546e5e60d936ac388ccc5
Benjamin Florin <benjamin.florin@gmail.com>
2014-09-05 13:58:27 -0400

    routes had extra path layer

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -3,12 +3,13 @@
 // Declare app level module which depends on filters, and services
 (function () {
     angular.module('myApp', [
-        'ngRoute',
-        'myApp.controllers',
-        'ui.bootstrap'
-    ]).
-        config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/bento', {templateUrl: 'partials/bento.html', controller: 'bento'});
-            $routeProvider.otherwise({templateUrl: 'partials/typeahead', controller: 'bento'});
-        }]);
+            'ngRoute',
+            'myApp.controllers',
+            'ui.bootstrap'
+        ]).
+        config(function ($routeProvider, $locationProvider) {
+            $routeProvider.when('/search', {templateUrl: 'search/partials/bento.html', controller: 'bento'});
+            $routeProvider.otherwise({templateUrl: 'search/partials/typeahead.html', controller: 'bento'});
+            $locationProvider.html5Mode(true);
+        });
 })();
