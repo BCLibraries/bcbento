eb4826290991fc66a7c19986b35f72788cd70361
Benjamin Florin <benjamin.florin@gmail.com>
2014-09-15 16:39:04 -0400

    Updating to lastest AngularJS 1.2.23. Fixes #10

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -84,7 +84,7 @@
                 $scope.getLocation = function (val) {
                     var truncate_length = 50;
 
-                    return $http.jsonp(location.protocol + '//' + search_server + '/search-services/suggest?callback=JSON_CALLBACK&text=' + val).then(function (res) {
+                    return $http.jsonp(location.protocol + '//' + search_server + '/search-services/suggest?callback=JSON_CALLBACK&text=' + val, {'cache': true}).then(function (res) {
 
                         // Autopopulate first item
                         var first_item = document.getElementById('searchbox').value;
