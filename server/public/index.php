1a9c61cf01e4a10181b6410117024caabe6ded6f
Benjamin Florin <benjamin.florin@gmail.com>
2015-02-26 13:01:03 -0500

    Add Primo redirects

--- server/public/index.php
+++ server/public/index.php
@@ -2,12 +2,15 @@
 
 use BCLib\BCBento\Cache;
 use BCLib\BCBento\JSONPWrapper;
+use BCLib\PrimoServices\DeepLink;
+use BCLib\PrimoServices\QueryTerm;
+use Slim\Slim;
 
 require_once('../vendor/autoload.php');
 
 $config = require('../config/.env.production.php');
 
-$app = new \Slim\Slim($config);
+$app = new Slim($config);
 
 require_once('../factories.php');
 require_once('../errors.php');
@@ -37,9 +40,42 @@ foreach ($paths as $path => $ttl) {
     )->ttl = $ttl;
 }
 
+$app->get(
+    '/primo-catalog',
+    function () use ($app) {
+        redirectToPrimo($app, $app->deeplink);
+    }
+);
+
+$app->get(
+    '/primo-articles',
+    function () use ($app) {
+        redirectToPrimo($app, $app->deeplink, true);
+    }
+);
+
 $app->add(new Cache($app->redis));
 $app->add(new JSONPWrapper());
 $app->run();
+
+function redirectToPrimo(Slim $app, DeepLink $dl, $articles = false)
+{
+    if ($articles) {
+        $scope = 'pci';
+        $tab = 'pci_only';
+    } else {
+        $scope = 'bcl';
+        $tab = 'bcl_only';
+    }
+
+    $term = new QueryTerm();
+    $term->keyword($app->request->params('any'));
+
+    $dl->onCampus('TRUE');
+    $dl->view('bclib');
+    $dl->group('GUEST');
+    $app->redirect('http://' . $dl->search($term) . "&search_scope=$scope&tab=$tab");
+}
 
 /**
  * Get seconds until 3am
