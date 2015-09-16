c2d5c90e3bba36defa0406c28c26c1e5a771a1ec
Benjamin Florin <benjamin.florin@gmail.com>
2015-09-16 12:12:43 -0400

    Better search styling and truncate long terms

--- client/app/js/bclib-typeahead.js
+++ client/app/js/bclib-typeahead.js
@@ -25,8 +25,19 @@ $(document).ready(function () {
         templates: {
             empty: '',
             suggestion: function (result) {
-                return '<div>' + result.value + ' <span class="typeahead-type">' + result.type + '</span></div>';
-            }
+                var display = truncate(result.value,80);
+                //result.value = display.replace(/&hellip;$/,'');
+                return '<div><span class="summary">' + display + '</span><span class="full">' + result.value + '</span></div>';
+            },
+            header: '<h3>Search suggestions</h3>'
         }
     });
+
+    function truncate(str, length) {
+        var too_long, s_;
+        too_long = str.length > length;
+        s_ = too_long ? str.substr(0, length - 1) : str;
+        s_ = too_long ? s_.substr(0, s_.lastIndexOf(' ')) : s_;
+        return too_long ? s_ + '&hellip;' : s_;
+    }
 });
