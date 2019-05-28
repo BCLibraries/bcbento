518522ccfea6b63917da1d14456f82ff91a931ea
Benjamin Florin <benjamin.florin@gmail.com>
2019-05-28 17:44:45 -0400

    Fix strange capital H probelm with cURL

--- server/src/FAQService.php
+++ server/src/FAQService.php
@@ -6,6 +6,7 @@ class FAQService implements ServiceInterface
 {
     public function fetch($keyword)
     {
+        $keyword = strtolower($keyword);
         $curl = curl_init();
         curl_setopt_array(
             $curl,
